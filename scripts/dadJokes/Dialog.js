4075215c761b03793838a6561b6911e6462ee910
Alex Curnow <alexcurnow@gmail.com>
2020-03-05 14:01:23 -0600

    finished v1 of dad jokes

--- scripts/dadJokes/Dialog.js
+++ scripts/dadJokes/Dialog.js
@@ -3,8 +3,9 @@ export const InitializeGiveUpButtonEvents = () => {
   allCloseButtons.forEach(button => button.addEventListener('click', event => event.target.parentNode.close()))
 
   const allGiveUpButtons = document.querySelectorAll('.giveUp')
+  
   allGiveUpButtons.forEach(button => button.addEventListener('click', event => {
-    const dialogSiblingSelector = `#${event.target.id}+dialog`
+    const dialogSiblingSelector = `${event.target.id}+dialog`
     const theDialog = document.querySelector(dialogSiblingSelector)
     theDialog.showModal()
   }))
