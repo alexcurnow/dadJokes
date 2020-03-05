24b4c563b04cdcfae0da50636234d9b0f8c7e11c
Alex Curnow <alexcurnow@gmail.com>
2020-03-05 15:31:52 -0600

    finished mvp for v2 - randomly generating jokes

--- scripts/dadJokes/DadJokesList.js
+++
@@ -1,9 +0,0 @@
-import { useDadJokesData } from "./DadJokesDataProvider.js"
-import { DadJoke } from "./DadJoke.js"
-
-const contentElement = document.querySelector('.dadJokesContainer')
-
-export const RenderDadJokesList = () => {
-  const dadJokesData = useDadJokesData()
-  dadJokesData.forEach(joke => contentElement.innerHTML += DadJoke(joke) )
-}
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,6 @@
-// import { RenderDadJokesList } from "./dadJokes/DadJokesList.js";
-import { InitializeGiveUpButtonEvents } from "./dadJokes/Dialog.js";
-import { RandomDadJokeGenerator } from "./dadJokes/RandomDadJokeGenerator.js";
-
-// RenderDadJokesList()
+import { InitializeGiveUpButtonEvents } from './dadJokes/Dialog.js'
+import { RandomDadJokeGenerator } from './dadJokes/RandomDadJokeGenerator.js'
 
 RandomDadJokeGenerator()
 
 InitializeGiveUpButtonEvents()
-
